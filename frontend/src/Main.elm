b2ed970d1c01013ecf09c287608673f92d4ceaa8
Bradley J. Spaulding <brad.spaulding@gmail.com>
2020-05-26 18:35:46 -0700

    moar frontend stuff

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -6,6 +6,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput, onSubmit)
 import Json.Decode as D
+import Json.Decode.Pipeline as DP
 import Json.Encode as E
 
 
@@ -25,6 +26,27 @@ createRoom =
 joinRoom : RoomId -> Cmd msg
 joinRoom roomId =
     E.object [ ( "type", E.string "join-room" ), ( "payload", E.string roomId ) ]
+        |> E.encode 0
+        |> sendMessage
+
+
+startGame : Cmd msg
+startGame =
+    E.object [ ( "type", E.string "start-game" ) ]
+        |> E.encode 0
+        |> sendMessage
+
+
+submitClue : String -> Cmd msg
+submitClue clue =
+    E.object [ ( "type", E.string "submit-clue" ), ( "payload", E.string clue ) ]
+        |> E.encode 0
+        |> sendMessage
+
+
+submitGuess : String -> Cmd msg
+submitGuess clue =
+    E.object [ ( "type", E.string "submit-guess" ), ( "payload", E.string clue ) ]
         |> E.encode 0
         |> sendMessage
 
@@ -51,7 +73,9 @@ wsMessageDecoder type_ =
             D.map2 PlayerJoinedRoom (D.field "connId" D.string) (D.field "name" D.string)
 
         "JoinRoomResponse" ->
-            D.map JoinedRoomResponse (D.field "roomId" D.string)
+            D.map2 JoinedRoomResponse
+                (D.field "roomId" D.string)
+                (D.field "playerNamesById" (D.dict D.string))
 
         "NewWordResponse" ->
             D.map NewWordResponse (D.field "word" D.string)
@@ -59,8 +83,45 @@ wsMessageDecoder type_ =
         "PlayerNameChanged" ->
             D.map2 PlayerNameChanged (D.field "connId" D.string) (D.field "name" D.string)
 
+        "GameUpdated" ->
+            D.succeed GameUpdated
+                |> DP.requiredAt [ "payload", "teamA" ] (D.list D.string)
+                |> DP.requiredAt [ "payload", "teamB" ] (D.list D.string)
+                |> DP.requiredAt [ "payload", "teamAGuesser" ] D.string
+                |> DP.requiredAt [ "payload", "teamBGuesser" ] D.string
+                |> DP.requiredAt [ "payload", "teamAClueGiver" ] D.string
+                |> DP.requiredAt [ "payload", "teamBClueGiver" ] D.string
+                |> DP.requiredAt [ "payload", "word" ] D.string
+                |> DP.requiredAt [ "payload", "clues" ] (D.list D.string)
+                |> DP.requiredAt [ "payload", "guesses" ] (D.list D.string)
+                |> DP.requiredAt [ "payload", "teamAScore" ] D.int
+                |> DP.requiredAt [ "payload", "teamBScore" ] D.int
+                |> DP.requiredAt [ "payload", "possession" ] possessionDecoder
+
         _ ->
             D.fail <| "Unknown message type '" ++ type_ ++ "'"
+
+
+type Possession
+    = TeamA
+    | TeamB
+
+
+possessionDecoder : D.Decoder Possession
+possessionDecoder =
+    D.string
+        |> D.andThen
+            (\s ->
+                case s of
+                    "TeamA" ->
+                        D.succeed TeamA
+
+                    "TeamB" ->
+                        D.succeed TeamB
+
+                    _ ->
+                        D.fail <| "Couldn't get a Possession from '" ++ s ++ "'"
+            )
 
 
 type alias RoomId =
@@ -76,9 +137,10 @@ type WSMessage
     | IdentifyConnection ConnId
     | CreateRoomResponse RoomId
     | PlayerJoinedRoom ConnId String
-    | JoinedRoomResponse RoomId
+    | JoinedRoomResponse RoomId (Dict ConnId String)
     | PlayerNameChanged ConnId String
     | NewWordResponse String
+    | GameUpdated (List ConnId) (List ConnId) ConnId ConnId ConnId ConnId String (List String) (List String) Int Int Possession
 
 
 
@@ -91,6 +153,22 @@ type alias Player =
     }
 
 
+type alias PasswordGame =
+    { teamA : List ConnId
+    , teamB : List ConnId
+    , teamAClueGiver : ConnId
+    , teamBClueGiver : ConnId
+    , teamAGuesser : ConnId
+    , teamBGuesser : ConnId
+    , word : String
+    , clues : List String
+    , guesses : List String
+    , teamAScore : Int
+    , teamBScore : Int
+    , possession : Possession
+    }
+
+
 type alias Model =
     { connId : Maybe String
     , name : String
@@ -99,6 +177,9 @@ type alias Model =
     , roomId : Maybe RoomId
     , word : Maybe String
     , tmpRoomId : String
+    , game : Maybe PasswordGame
+    , tempClue : String
+    , tempGuess : String
     }
 
 
@@ -118,6 +199,9 @@ init =
       , roomId = Nothing
       , word = Nothing
       , tmpRoomId = ""
+      , game = Nothing
+      , tempClue = ""
+      , tempGuess = ""
       }
     , Cmd.none
     )
@@ -137,14 +221,17 @@ playerNameMessage name =
 
 
 type Msg
-    = NoOp
-    | Send
-    | PlayerNameUpdated String
+    = PlayerNameUpdated String
     | PlayerNameSubmitted
     | UpdateRoomId String
     | CreateRoom
     | JoinRoom
+    | StartGame
     | Recv String
+    | SubmitClue
+    | ClueUpdated String
+    | SubmitGuess
+    | GuessUpdated String
 
 
 handleWsMessage : Model -> WSMessage -> ( Model, Cmd Msg )
@@ -166,14 +253,33 @@ handleWsMessage model1 msg =
         PlayerJoinedRoom connId name ->
             ( { model | playersById = Dict.insert connId (Player connId name) model.playersById }, Cmd.none )
 
-        JoinedRoomResponse roomId ->
-            ( { model | roomId = Just roomId }, Cmd.none )
+        JoinedRoomResponse roomId playerNamesById ->
+            ( { model | roomId = Just roomId, playersById = playerNamesById |> Dict.toList |> List.map (\( connId, name ) -> ( connId, Player connId name )) |> Dict.fromList }, Cmd.none )
 
         CreateRoomResponse roomId ->
             ( { model | roomId = Just roomId }, Cmd.none )
 
         NewWordResponse word ->
             ( { model | word = Just word }, Cmd.none )
+
+        GameUpdated teamA teamB teamAGuesser teamBGuesser teamAClueGiver teamBClueGiver word clues guesses teamAScore teamBScore possession ->
+            let
+                game =
+                    { teamA = teamA
+                    , teamB = teamB
+                    , teamAGuesser = teamAGuesser
+                    , teamBGuesser = teamBGuesser
+                    , teamAClueGiver = teamAClueGiver
+                    , teamBClueGiver = teamBClueGiver
+                    , word = word
+                    , clues = clues
+                    , guesses = guesses
+                    , teamAScore = teamAScore
+                    , teamBScore = teamBScore
+                    , possession = possession
+                    }
+            in
+            ( { model | game = Just game }, Cmd.none )
 
         ErrorResponse _ ->
             ( model, Cmd.none )
@@ -182,12 +288,6 @@ handleWsMessage model1 msg =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-        Send ->
-            ( model, sendMessage "foo" )
-
         PlayerNameUpdated name ->
             ( { model | name = name }, Cmd.none )
 
@@ -203,6 +303,21 @@ update msg model =
         JoinRoom ->
             ( model, joinRoom model.tmpRoomId )
 
+        StartGame ->
+            ( model, startGame )
+
+        ClueUpdated clue ->
+            ( { model | tempClue = clue }, Cmd.none )
+
+        SubmitClue ->
+            ( { model | tempClue = "" }, submitClue model.tempClue )
+
+        GuessUpdated clue ->
+            ( { model | tempGuess = clue }, Cmd.none )
+
+        SubmitGuess ->
+            ( { model | tempGuess = "" }, submitGuess model.tempGuess )
+
         Recv message ->
             case D.decodeString socketDecoder message of
                 Ok decodedMessage ->
@@ -214,6 +329,118 @@ update msg model =
 
 
 ---- VIEW ----
+
+
+areWeAGuesser : Model -> Bool
+areWeAGuesser model =
+    case ( model.connId, model.game ) of
+        ( Just connId, Just game ) ->
+            game.teamAGuesser == connId || game.teamBGuesser == connId
+
+        _ ->
+            False
+
+
+isMyTeamGuessing : Model -> Bool
+isMyTeamGuessing model =
+    case ( model.connId, model.game ) of
+        ( Just connId, Just game ) ->
+            case ( myTeam connId game, currentTeamGuessing game ) of
+                ( Just team, Just guessingTeam ) ->
+                    team == guessingTeam
+
+                _ ->
+                    False
+
+        _ ->
+            False
+
+
+currentTeamGuessing : PasswordGame -> Maybe Possession
+currentTeamGuessing game =
+    if List.length game.clues == 1 + List.length game.guesses then
+        if modBy 2 (List.length game.guesses) == 0 then
+            Just game.possession
+
+        else
+            togglePossession game.possession |> Just
+
+    else
+        Nothing
+
+
+amIGuessing : Model -> Bool
+amIGuessing model =
+    areWeAGuesser model && isMyTeamGuessing model
+
+
+togglePossession : Possession -> Possession
+togglePossession team =
+    case team of
+        TeamA ->
+            TeamB
+
+        TeamB ->
+            TeamA
+
+
+currentTeamGivingClue : PasswordGame -> Maybe Possession
+currentTeamGivingClue game =
+    if List.length game.clues == List.length game.guesses then
+        if modBy 2 (List.length game.clues) == 0 then
+            Just game.possession
+
+        else
+            togglePossession game.possession |> Just
+
+    else
+        Nothing
+
+
+myTeam : ConnId -> PasswordGame -> Maybe Possession
+myTeam connId game =
+    case ( List.member connId game.teamA, List.member connId game.teamB ) of
+        ( True, _ ) ->
+            Just TeamA
+
+        ( _, True ) ->
+            Just TeamB
+
+        _ ->
+            Nothing
+
+
+isMyTeamGivingAClue : ConnId -> PasswordGame -> Bool
+isMyTeamGivingAClue connId game =
+    case ( myTeam connId game, currentTeamGivingClue game ) of
+        ( Just team, Just clueGivingTeam ) ->
+            team == clueGivingTeam
+
+        _ ->
+            False
+
+
+amITheClueGiverForMyTeam : ConnId -> PasswordGame -> Bool
+amITheClueGiverForMyTeam connId game =
+    case myTeam connId game of
+        Just TeamA ->
+            game.teamAClueGiver == connId
+
+        Just TeamB ->
+            game.teamBClueGiver == connId
+
+        Nothing ->
+            False
+
+
+amIGivingAClue : Model -> Bool
+amIGivingAClue model =
+    case ( model.connId, model.game ) of
+        ( Just connId, Just game ) ->
+            isMyTeamGivingAClue connId game && amITheClueGiverForMyTeam connId game
+
+        _ ->
+            False
 
 
 wsMessageToString : WSMessage -> String
@@ -231,8 +458,8 @@ wsMessageToString msg =
         PlayerJoinedRoom connId name ->
             "PlayerJoinedRoom(" ++ connId ++ ", \"" ++ name ++ "\")"
 
-        JoinedRoomResponse roomId ->
-            "JoinedRoomResponse(" ++ roomId ++ ")"
+        JoinedRoomResponse roomId _ ->
+            "JoinedRoomResponse(roomId = " ++ roomId ++ ")"
 
         PlayerNameChanged connId name ->
             "PlayerNameChanged(" ++ connId ++ ", \"" ++ name ++ "\")"
@@ -240,15 +467,20 @@ wsMessageToString msg =
         NewWordResponse word ->
             "NewWordResponse(" ++ word ++ ")"
 
+        GameUpdated _ _ _ _ _ _ _ _ _ _ _ _ ->
+            "GameUpdated(<see debug state>)"
+
 
 view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
         , h1 [] [ text "Your Elm App is working!" ]
+
+        -- , audio [ src "/password-thinking-song.m4a", autoplay True, controls True, loop True ] []
         , case playerName model of
             Nothing ->
-                Html.form [ onSubmit PlayerNameSubmitted ]
+                Html.form [ id "player-name", onSubmit PlayerNameSubmitted ]
                     [ input [ type_ "text", value model.name, onInput PlayerNameUpdated ] []
                     , input [ type_ "submit", value "Submit" ] []
                     ]
@@ -271,10 +503,51 @@ view model =
                                 ]
 
                         Just roomId ->
-                            div [] [ text ("In room " ++ roomId) ]
+                            div []
+                                [ text ("In room " ++ roomId)
+                                , case model.game of
+                                    Nothing ->
+                                        button [ onClick StartGame ] [ text "Start Game" ]
+
+                                    Just game ->
+                                        div []
+                                            [ h2 [] [ text "Team A" ]
+                                            , ul [] (List.map (playerNameLi model) game.teamA)
+                                            , h2 [] [ text "Team B" ]
+                                            , ul [] (List.map (playerNameLi model) game.teamB)
+                                            , h2 [] [ text "Clues" ]
+                                            , ul [] (List.map (\s -> li [] [ text s ]) game.clues)
+                                            , h2 [] [ text "Guesses" ]
+                                            , ul [] (List.map (\s -> li [] [ text s ]) game.guesses)
+                                            , if amIGuessing model then
+                                                div []
+                                                    [ text "You are guessing!"
+                                                    , Html.form [ onSubmit SubmitGuess ] [ input [ type_ "text", value model.tempGuess, onInput GuessUpdated ] [] ]
+                                                    ]
+
+                                              else
+                                                div []
+                                                    [ h2 [] [ text "Current Word" ]
+                                                    , div [] [ text game.word ]
+                                                    ]
+                                            , if amIGivingAClue model then
+                                                div []
+                                                    [ text "Your turn to give a clue!"
+                                                    , Html.form [ onSubmit SubmitClue ] [ input [ type_ "text", value model.tempClue, onInput ClueUpdated ] [] ]
+                                                    ]
+
+                                              else
+                                                div [] []
+                                            ]
+                                ]
                     ]
         , pre [] [ code [] (List.indexedMap (\i m -> String.fromInt i ++ " " ++ m ++ "\n" |> text) (List.map wsMessageToString model.messages)) ]
         ]
+
+
+playerNameLi : Model -> ConnId -> Html Msg
+playerNameLi model connId =
+    li [] [ text <| .name <| Maybe.withDefault (Player "" "Unknown") <| Dict.get connId model.playersById ]
 
 
 
