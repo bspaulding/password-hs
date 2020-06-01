bd06d8d10a413fa5ed8ac98d842d16fda87f9471
Bradley J. Spaulding <brad.spaulding@gmail.com>
2020-05-31 20:51:59 -0700

    Taking port from env

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,18 @@ module Main where
 
 import Network.Wai.Handler.Warp (run)
 import Password.Server (mkApp)
+import System.Environment
+
+getPort :: IO Int
+getPort = do
+  sysPort <- lookupEnv "PORT"
+  return $ case sysPort of
+              Nothing -> 8080
+              Just portStr -> read portStr :: Int
 
 main :: IO ()
 main = do
   app <- mkApp
-  print "http://localhost:8080"
-  run 8080 app
+  port <- getPort
+  print $ "http://localhost:" ++ show port
+  run port app
